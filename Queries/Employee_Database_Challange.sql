5aa88ba8e2259bd96f1052c48b449b5504f4c321
Matt Goldstein <101226991+Martamorningham@users.noreply.github.com>
2022-05-15 23:59:13 -0600

    commit did not work, so adding deliverable 2 here.

--- Queries/Employee_Database_Challange.sql
+++ Queries/Employee_Database_Challange.sql
@@ -17,17 +17,20 @@ WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY emp_no;
 
 -- Check the table
-SELECT * FROM retiring_titles;
+SELECT * FROM retirement_titles;
 
 -- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
-	rt.first_name,
-	rt.last_name,
-	rt.title
-INTO unique_titles
-FROM retirement_titles AS rt
-WHERE (rt.to_date BETWEEN '1952-01-01' AND '1999-01-01')
-ORDER BY emp_no ASC, to_date DESC;
+SELECT DISTINCT ON (emp_no) emp_no,
+	first_name,
+	last_name,
+	title
+--INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no ASC, title DESC;
+
+
+-- Check the table
+SELECT * FROM unique_titles;
 
 --find count of titles using select count on UT
 SELECT COUNT(ut.emp_no),ut.title
@@ -35,3 +38,25 @@ INTO retiring_titles
 FROM unique_titles as ut
 GROUP BY title 
 ORDER BY COUNT(title) DESC;
+--SELECT * FROM retiring_titles;
+--Deliverable 2: The Employees Eligible for the Mentorship Program
+--reuse the hiring code from module and join employeees, dept_emp, and titles csvs
+SELECT DISTINCT ON(e.emp_no)e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+t.title
+
+INTO mentor_eligibility
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE (de.to_date = '9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no;
+-- Check the table
+SELECT * FROM mentor_eligibility;
